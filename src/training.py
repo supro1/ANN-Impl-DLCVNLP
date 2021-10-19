1a12db4d58460954738acedb2de17d3a505ba2c9
supro1 <bikproml@rediffmail.com>
2021-10-19 21:49:22 +0530

    training stage done

--- src/training.py
+++ src/training.py
@@ -1,6 +1,8 @@
 from  src.utils.common import read_config
 import argparse
-from src.utils.data_mgmt import get_data
+from src.utils.data_mgmt import get_data, save_model
+from src.utils.model import create_model
+import os
 
 def training(config_path):
     config=read_config(config_path)
@@ -9,6 +11,28 @@ def training(config_path):
     
     (X_train, y_train), (X_valid, y_valid), (X_test, y_test)=get_data(validation_data_size)
 
+    LOSS_FUNCTION=config['params']['loss_function']
+    OPTIMIZER=config['params']['optimizer']
+    METRICS=config['params']['metrics']
+    NUM_CLASSES=config['params']['num_classes']
+
+    model = create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,NUM_CLASSES)
+
+    EPOCHS = config["params"]["epochs"]
+    VALIDATION_SET = (X_valid, y_valid)
+
+    history = model.fit(X_train, y_train, epochs=EPOCHS,
+                    validation_data=VALIDATION_SET)
+
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
+    
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+    
+    model_name = config["artifacts"]["model_name"]
+
+    save_model(model, model_name, model_dir_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
